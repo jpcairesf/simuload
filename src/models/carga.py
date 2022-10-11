8fce9a682526d6118301a95ffebc4c43c6b2ac57
caleo-hub <63008409+caleo-hub@users.noreply.github.com>
2022-10-11 16:51:36 -0300

    carga propreties added

--- src/models/carga.py
+++ src/models/carga.py
@@ -3,3 +3,19 @@ class Carga:
     def __init__(self,carga_id):
         self._carga_id = carga_id
         self._nome = None
+        self._equipamentos = []
+        
+    @property
+    def id(self):
+        return self._carga_id
+    
+    @property
+    def nome(self):
+        """Nome do Tipo de Carga"""
+        return self._nome
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+        
+    def adicionar_equipamentos(self, equipamento_id, quantidade):
+        self._equipamentos.append([equipamento_id,quantidade])

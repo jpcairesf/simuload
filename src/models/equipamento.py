b23822e750c8ee21dcecce659e04b2a731eb8759
caleo-hub <63008409+caleo-hub@users.noreply.github.com>
2022-10-11 16:00:25 -0300

    equipamento model created

--- src/models/equipamento.py
+++ src/models/equipamento.py
@@ -1,16 +1,18 @@
-import numpy as np
-from 
+from utils.parsers import horario_parse
+
 class Equipamento: 
     
-    def __init__(self,carga_id:int):
+    def __init__(self,equipamento_id):
+        self._equipamento_id = equipamento_id
         self._nome = None
-        self._carga_id = carga_id
-        self._equipamento_id = None
         self._potencia = None
         self._fator_potencia = None
         self._uso_diario = None
-        self._quantidade = None    
-        
+    
+    @property
+    def id(self):
+        return self._equipamento_id
+    
     @property
     def nome(self):
         """Nome do Tipo de Equipamento"""
@@ -18,20 +20,10 @@ class Equipamento:
     @nome.setter
     def nome(self, nome):
         self._nome = nome
-    
-    @property
-    def carga_id(self):
-        return self._carga_id
-    
-    @property
-    def equipamento_id(self):
-        return self._equipamento_id
-    @equipamento_id.setter
-    def equipamento_id(self,id):
-        self._equipamento_id = id
         
     @property
     def potencia(self):
+        """Potência Elétrica do Equipamento em Watts"""
         return self._potencia
     @potencia.setter
     def potencia(self,potencia:float):
@@ -39,6 +31,7 @@ class Equipamento:
         
     @property
     def fator_potencia(self):
+        """Fator de Potência Elétrica do Equipamento"""
         return self._fator_potencia
     @fator_potencia.setter
     def fator_potencia(self,fator_potencia:float):
@@ -49,39 +42,12 @@ class Equipamento:
         
     @property
     def uso_diario(self):
+        """Horas em que o equipamento são usados"""
         return self._uso_diario
     @uso_diario.setter
     def uso_diario(self,uso_diario:str):
+        self._uso_diario = horario_parse(uso_diario)
         
-        faixas_horarios = uso_diario.split(',')
-        horarios = [faixa.split(':') for faixa in faixas_horarios]
-        
-        uso_diario = np.zeros((24))
-        
-        for faixa in horarios:
-            start = int(faixa[0])
-            end = int(faixa[-1])
-            uso_diario[start:end+1] = 1
-        
-        self._uso_diario = uso_diario
-        
-    @property
-    def quantidade(self):
-        return self._quantidade
-    @quantidade.setter
-    def quantidade(self,quantidade: int):
-        if quantidade > 0:
-            self._quantidade = quantidade
-    
-    
-    
-    
-if __name__=='__main__':
-    lampada = Equipamento(1234)
-    print(lampada._carga_id)
-    print(lampada._nome)
-    lampada.nome = 'Lampada 60W'
-    print(lampada.nome)
     
 
         

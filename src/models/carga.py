fb1d8de3f5d03472e5b7437f269177c17e88ef64
Joao Caires <jpcairesf@gmail.com>
2022-10-15 18:32:32 -0300

    Adicionando atributos que faltavam

--- src/models/carga.py
+++ src/models/carga.py
@@ -4,6 +4,7 @@ class Carga:
         self._carga_id = carga_id
         self._nome = None
         self._equipamentos = []
+        self._quantidade = 0
         
     @property
     def id(self):
@@ -13,9 +14,19 @@ class Carga:
     def nome(self):
         """Nome do Tipo de Carga"""
         return self._nome
+
     @nome.setter
     def nome(self, nome):
         self._nome = nome
         
     def adicionar_equipamentos(self, equipamento_id, quantidade):
         self._equipamentos.append([equipamento_id,quantidade])
+
+    @property
+    def quantidade(self):
+        """Quantidade da Carga na simulação"""
+        return self._quantidade
+
+    @quantidade.setter
+    def quantidade(self, quantidade):
+        self._quantidade = quantidade

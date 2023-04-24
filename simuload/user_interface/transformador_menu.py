e5885a286e37b133ed732011cc1459f05216cfa8
Joao Caires <jpcairesf@gmail.com>
2023-04-23 21:37:07 -0300

    fix: working correctly

--- simuload/user_interface/transformador_menu.py
+++ simuload/user_interface/transformador_menu.py
@@ -27,6 +27,7 @@ class TransformadorMenu(QWidget):
         transf = self.ui.procuraTransf.text()
         self.transformadores = self.service.consultar_transformador(transf)
         self.add_itens()
+        self.parent.get_transformadores()
 
     def add_itens(self):
         self.ui.transformadorLista.clear()
@@ -42,11 +43,12 @@ class TransformadorMenu(QWidget):
         transf_id = self.get_selected_transf()
         self.service.remover_transformador(transf_id)
         self.get_transformadores()
-        self.parent.get_transformadores()
 
     def create_transf(self):
         self.widget = TransformadorWindow(self)
         self.widget.show()
+        self.get_transformadores()
+
 
     def edit_transf(self):
         transf_id = self.get_selected_transf()
@@ -55,3 +57,4 @@ class TransformadorMenu(QWidget):
         self.widget = TransformadorWindow(parent=self, edit=transf_id)
         self.widget.set_input_info(transformador)
         self.widget.show()
+        self.get_transformadores()
